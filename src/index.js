65d6c9508baadbaace3882417468d31ce51dfc70
jjader <jamisson.junior@a3data.com.br>
2022-08-24 19:09:44 -0300

    feat: add chat

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
@@ -18,6 +19,7 @@ io.on('connection', (socket) => {
 
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
     console.log('message: ' + msg);
   });
 });
